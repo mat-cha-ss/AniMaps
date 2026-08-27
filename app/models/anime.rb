bc923bfa0e1b30c4cdcf4ed1d6ab7a465449da7a
Mat Mapua <dmatman864@gmail.com>
2026-08-28 02:22:28 +0900

    add anime and series relationships to models

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,3 +1,9 @@
 class Anime < ApplicationRecord
   belongs_to :series, optional: true
+
+  has_many :anime_locations, dependent: :destroy
+  has_many :locations, through: :anime_locations
+
+  has_many :anime_events, dependent: :destroy
+  has_many :events, through: :anime_events
 end
